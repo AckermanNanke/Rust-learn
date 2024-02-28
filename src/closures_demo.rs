fb5017c7a74d99fa55cb2795fbf20916cfa57c2a
AckermanNan <1178809645@qq.com>
2024-02-28 20:30:33 +0800

    学习

---
+++ src/closures_demo.rs
@@ -0,0 +1,31 @@
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+struct Inventtory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventtory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+        for i in &self.shirts {
+            match i {
+                Red => num_red += 1,
+                Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
+
+pub fn run_closures_demo() {}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use crate::closures_demo::run_closures_demo;
 use crate::generics::run_generics_demo;
 use crate::io_demo::run_io_demo1;
 use crate::lifetime_demo::run_lifetime_demo;
 use crate::trait_demo::run_trait_demo;
+mod closures_demo;
 mod generics;
 mod io_demo;
 mod lifetime_demo;
@@ -12,5 +14,6 @@ fn main() {
     // run_generics_demo();
     // run_trait_demo();
     // run_lifetime_demo();
-    run_io_demo1();
+    // run_io_demo1();
+    run_trait_demo();
 }
