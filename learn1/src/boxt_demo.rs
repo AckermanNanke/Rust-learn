c0c3f9bebf9e96d9dfb023ebd4627d1da5edb65b
AckermanNan <1178809645@qq.com>
2024-03-20 13:01:50 +0800

    drop类使用

--- learn1/src/boxt_demo.rs
+++ learn1/src/boxt_demo.rs
@@ -9,6 +9,12 @@ enum List {
 // 实现deref强制转换
 struct DemoBox<T>(T);
 
+// 实现销毁数据
+#[derive(Debug)]
+struct DropBox {
+    data: String,
+}
+
 impl<T> Deref for DemoBox<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
@@ -22,15 +28,30 @@ impl<T> DemoBox<T> {
     }
 }
 
-// 实现deref强制转换测试
-fn run_boxt_demo2() {
-    let x = 5;
-    let y = DemoBox::new(x);
-    let z = &x;
+impl Drop for DropBox {
+    fn drop(&mut self) {
+        println!("Dropping DropBox with data `{:#?}`!", self.data);
+    }
+}
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-    assert_eq!(5, *z);
+// 实现deref强制转换测试
+fn run_boxt_demo_deref() {
+    let x = String::from("5");
+    let y = DemoBox::new(x);
+    let z = &y;
+
+    assert_eq!("5", *y);
+}
+fn run_boxt_demo_drop() {
+    let c = DropBox {
+        data: String::from("my stuff"),
+    };
+    let d: DropBox = DropBox {
+        data: String::from("other stuff"),
+    };
+    drop(c);
+    println!("Dropping DropBox with data `{:#?}`!", c);
+    println!("Dropping DropBox with data `{:#?}`!", d);
 }
 
 // 堆数据
@@ -40,6 +61,7 @@ fn run_boxt_demo1() {
 }
 
 pub fn run_boxt_demo() {
-    // run_boxt_demo1();
-    run_boxt_demo2();
+    run_boxt_demo1();
+    run_boxt_demo_deref();
+    run_boxt_demo_drop();
 }
--- learn1/src/main.rs
+++ learn1/src/main.rs
@@ -1,17 +1,17 @@
 use crate::boxt_demo::run_boxt_demo;
-use crate::closures_demo::run_closures_demo;
-use crate::generics::run_generics_demo;
-use crate::io_demo::run_io_demo1;
-use crate::iterator_demo::run_iterator_demo;
-use crate::lifetime_demo::run_lifetime_demo;
-use crate::trait_demo::run_trait_demo;
+// use crate::closures_demo::run_closures_demo;
+// use crate::generics::run_generics_demo;
+// use crate::io_demo::run_io_demo1;
+// use crate::iterator_demo::run_iterator_demo;
+// use crate::lifetime_demo::run_lifetime_demo;
+// use crate::trait_demo::run_trait_demo;
 mod boxt_demo;
-mod closures_demo;
-mod generics;
-mod io_demo;
-mod iterator_demo;
-mod lifetime_demo;
-mod trait_demo;
+// mod closures_demo;
+// mod generics;
+// mod io_demo;
+// mod iterator_demo;
+// mod lifetime_demo;
+// mod trait_demo;
 
 fn main() {
     // run_learn_demo();
