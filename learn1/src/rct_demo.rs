a489f3ca9401706fe3547205c39e44f1f998d279
AckermanNan <1178809645@qq.com>
2024-03-28 22:56:33 +0800

    学习

--- learn1/src/rct_demo.rs
+++ learn1/src/rct_demo.rs
@@ -15,27 +15,41 @@ use crate::rct_demo::List::{Cons, Nil};
 //     let c = Cons(4, Box::new(&a));
 // }
 
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil,
+// }
+
+// // 需要使用 use 语句将 Rc<T> 引入作用域，因为它不在 prelude 中。在 main 中创建了存放 5 和 10 的列表并将其存放在 a 的新的 Rc<List> 中。接着当创建 b 和 c 时，调用 Rc::clone 函数并传递 a 中 Rc<List> 的引用作为参数。
+// // 也可以调用 a.clone() 而不是 Rc::clone(&a)，不过在这里 Rust 的习惯是使用 Rc::clone。Rc::clone 的实现并不像大部分类型的 clone 实现那样对所有数据进行深拷贝。Rc::clone 只会增加引用计数，这并不会花费多少时间。深拷贝可能会花费很长时间。通过使用 Rc::clone 进行引用计数，可以明显的区别深拷贝类的克隆和增加引用计数类的克隆。当查找代码中的性能问题时，只需考虑深拷贝类的克隆而无需考虑 Rc::clone 调用。
+// fn run_rct_demo1() {
+//     let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+//     let b = Cons(3, Rc::clone(&a));
+//     let c = Cons(4, Rc::clone(&a));
+// }
+
+#[derive(Debug)]
 enum List {
-    Cons(i32, Rc<List>),
+    Cons(i32, RefCell<Rc<List>>),
     Nil,
+}
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
 }
 
 // 需要使用 use 语句将 Rc<T> 引入作用域，因为它不在 prelude 中。在 main 中创建了存放 5 和 10 的列表并将其存放在 a 的新的 Rc<List> 中。接着当创建 b 和 c 时，调用 Rc::clone 函数并传递 a 中 Rc<List> 的引用作为参数。
 // 也可以调用 a.clone() 而不是 Rc::clone(&a)，不过在这里 Rust 的习惯是使用 Rc::clone。Rc::clone 的实现并不像大部分类型的 clone 实现那样对所有数据进行深拷贝。Rc::clone 只会增加引用计数，这并不会花费多少时间。深拷贝可能会花费很长时间。通过使用 Rc::clone 进行引用计数，可以明显的区别深拷贝类的克隆和增加引用计数类的克隆。当查找代码中的性能问题时，只需考虑深拷贝类的克隆而无需考虑 Rc::clone 调用。
 fn run_rct_demo1() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
-}
-
-fn run_rct_demo2() {
     let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    println!("a 引用计数 = {}", Rc::strong_count(&a));
+    println!("a 下一项 = {:?}", a.tail());
 
-    println!("a initial rc count = {}", Rc::strong_count(&a));
-    println!("a next item = {:?}", a.tail());
-
-    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
-
+    let b = Rc::new(Cons(5, RefCell::new(Rc::clone(&a))));
     println!("a rc count after b creation = {}", Rc::strong_count(&a));
     println!("b initial rc count = {}", Rc::strong_count(&b));
     println!("b next item = {:?}", b.tail());
@@ -44,11 +58,12 @@ fn run_rct_demo2() {
         *link.borrow_mut() = Rc::clone(&b);
     }
 
-    println!("b rc count after changing a = {}", Rc::strong_count(&b));
     println!("a rc count after changing a = {}", Rc::strong_count(&a));
-}
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
 
+    //取消下一行的注释，看到我们有一个循环;它将溢出堆栈
+    // println!("a next item = {:?}", a.tail());
+}
 pub fn run_rct_demo() {
     run_rct_demo1();
-    run_rct_demo2();
 }
